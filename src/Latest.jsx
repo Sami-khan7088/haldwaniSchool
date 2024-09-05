f31c9f1087a82af42d171bce71d2dd88d7705404
Sami-khan7088 <89895044+Sami-khan7088@users.noreply.github.com>
2024-09-05 15:22:54 +0530

    my second commit

--- src/Latest.jsx
+++ src/Latest.jsx
@@ -3,25 +3,25 @@ import { FaNewspaper } from "react-icons/fa";
 const Latest = () => {
   return (
     <div className='MAincontainer3'>
-        <div className='LatestNews'>
+      <div className='LatestNews'>
         <div className='LatestNewsIcon'>
-        <button className='btnNews'>
-        <div className='newsIcon'>
-        <FaNewspaper style={{fontSize:"20px"}}/> 
-        </div>
-        <div className='newsName'>
-        Latest News
-        </div>
-        </button>
+          <button className='btnNews'>
+            <div className='newsIcon'>
+              <FaNewspaper style={{ fontSize: "20px" }} />
+            </div>
+            <div className='newsName'>
+              Latest News
+            </div>
+          </button>
         </div>
         <div className='News'>
-        <marquee>
-        <h1>1. Summer Vacation till 1st July 2023.
-2. School will reopen on 3rd July (Monday).
-3. No any change in arrival and departure timings.</h1>
-        </marquee>
+          <marquee>
+            <h1>1. Summer Vacation till 1st July 2023.
+              2. School will reopen on 3rd July (Monday).
+              3. No any change in arrival and departure timings.</h1>
+          </marquee>
         </div>
-        </div>
+      </div>
     </div>
   )
 }
